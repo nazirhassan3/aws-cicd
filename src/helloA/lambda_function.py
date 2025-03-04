990887c9c132874edee70306dce20f71ebbbbdfd
bilal <bilal.aqeel@proleadsoft.com>
2025-03-04 22:12:30 +0500

    Lamda updated

--- src/helloA/lambda_function.py
+++ src/helloA/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello A!!!')
+        'body': json.dumps('Hello re-A!!!')
     }
